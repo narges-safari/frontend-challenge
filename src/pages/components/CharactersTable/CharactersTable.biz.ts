18264fcafff3ca420e37e5e0c533ea7553fe717f
narges safari <narges.safari.8631@gmail.com>
2022-08-30 18:51:11 +0430

    fix: fix filter and search

--- src/pages/components/CharactersTable/CharactersTable.biz.ts
+++ src/pages/components/CharactersTable/CharactersTable.biz.ts
@@ -1,8 +1,8 @@
 import { GridSelectionModel } from "@mui/x-data-grid";
 import { useCallback, useEffect, useMemo, useState } from "react";
+import charactersJson from "../../../assets/data/characters.json";
 import { useChampionsContext } from "../../ChampionsSquad.context";
 import { Character, CharacterTag } from "../../ChampionsSquad.types";
-import charactersJson from "../../../assets/data/characters.json";
 
 const data: Character[] = charactersJson as Character[];
 
@@ -16,28 +16,31 @@ const useCharactersTable = () => {
     setSelectionModel,
   } = useChampionsContext();
   const [charactersData, setCharactersData] = useState<Character[]>(data);
-
+  const [isShowSnackbar, setIsShowSnackbar] = useState(false);
   const selectionModelHandler = (id: GridSelectionModel) => {
     setSelectionModel(id);
   };
 
-  const cellClickHandler = (row: Character) => {
-    let foundSelectedChampion = selectedChampions.find(
-      (item: Character) => item.id === row.id
-    );
-    if (foundSelectedChampion?.id) {
-      let newSelectedChampions = selectedChampions.filter(
-        (item: Character) => item.id !== foundSelectedChampion?.id
+  const cellClickHandler = useCallback(
+    (row: Character) => {
+      let foundSelectedChampion = selectedChampions.find(
+        (item: Character) => item.id === row.id
       );
-      setSelectedChampions(newSelectedChampions);
-    } else {
-      setSelectedChampions((_prev: Character[]) => [..._prev, row]);
-    }
-  };
+      if (foundSelectedChampion?.id) {
+        let newSelectedChampions = selectedChampions.filter(
+          (item: Character) => item.id !== foundSelectedChampion?.id
+        );
+        setSelectedChampions(newSelectedChampions);
+      } else {
+        setSelectedChampions((_prev: Character[]) => [..._prev, row]);
+      }
+    },
+    [selectedChampions, setSelectedChampions]
+  );
 
   const filterTagRegex = useMemo(() => {
-    let _preparedRegexString = tagFilter.join("|");
-    return new RegExp(_preparedRegexString, "gi");
+    let _preparedRegexString = tagFilter.join(")(?=.*");
+    return new RegExp(`(?=.*${_preparedRegexString})`, "gi");
   }, [tagFilter]);
 
   const isTagIncluded = useCallback(
@@ -52,25 +55,16 @@ const useCharactersTable = () => {
   );
 
   const filterData = useCallback(() => {
+    const searchText = search.trim();
+    if (!tagFilter.length && !searchText) {
+      setCharactersData(data);
+      return;
+    }
     const filteredData = data.filter((item: Character) => {
-      console.log(
-        tagFilter,
-        item.name.toLowerCase().includes(search.trim()),
-        isTagIncluded(item.tags)
+      if (!searchText) return isTagIncluded(item.tags);
+      return (
+        item.name.toLowerCase().includes(searchText) && isTagIncluded(item.tags)
       );
-      const searchText = search.trim();
-      if (!searchText) {
-        if (tagFilter?.length > 0) {
-          return isTagIncluded(item.tags);
-        } else {
-          return item.name.toLowerCase().includes(searchText);
-        }
-      } else {
-        return (
-          item.name.toLowerCase().includes(searchText) ||
-          isTagIncluded(item.tags)
-        );
-      }
     });
     setCharactersData(filteredData);
   }, [isTagIncluded, search, tagFilter]);
@@ -79,17 +73,25 @@ const useCharactersTable = () => {
     filterData();
   }, [filterData, tagFilter]);
 
-  useEffect(() => {
+  const updateMyTeam = useCallback(() => {
     if (myTeam) setCharactersData(selectedChampions);
     else {
       filterData();
     }
   }, [filterData, myTeam, selectedChampions]);
 
+  useEffect(() => {
+    updateMyTeam();
+  }, [myTeam, updateMyTeam]);
+
   return {
+    filterData,
+    updateMyTeam,
     charactersData,
     cellClickHandler,
+    isShowSnackbar,
     selectionModelHandler,
+    setIsShowSnackbar,
   };
 };
 
